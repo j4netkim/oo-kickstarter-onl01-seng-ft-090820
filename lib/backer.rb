587cc107190cd4ee454aa73cc899dd1f5ac218ec
J K <kj688895@gmail.com>
2020-09-26 00:59:22 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,8 +4,13 @@ class Backer
 
   def initialize(name)
     @name = name
-    @backed_projects = [] 
+    @backed_projects = []
   end
+
+  def back_project(project)
+    @backed_projects << project
+    project.backer << self
+  end 
 
 
 
